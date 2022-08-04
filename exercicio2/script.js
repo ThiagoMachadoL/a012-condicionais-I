7b04cecc8712cdc9c0d6aa05899d5b2cacaba1fb
ThiagoMachadoL <thiagojkp123@gmail.com>
2022-08-03 21:43:17 -0300

    Exercício de condiocionais

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,20 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = prompt('Qual a sua idade?');
+let terminouOEnsinoMedio = prompt('Você terminou o ensino médio?');
+let isCursandoOutraFaculdade = prompt('Você está cursando outra faculdade?');
 
-if(){
-
+if(idade >= 18){
+    console.log("A pessoa é maior de idade");
 }else{
-	
+	console.log("A pessoa é menor de idade");
 }
+if(terminouOEnsinoMedio === 'sim'&&'Sim'){
+    console.log('Sim')
+}else{
+	console.log('Não')
+}
+if(isCursandoOutraFaculdade === 'sim'&&'Sim'){
+    console.log('Sim')
+}else{
+	console.log('Não')
+}
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Escreva aqui sua nacionalidade")
+
+if(nacionalidade === "brasileira"){
+    console.log("A pessoa é brasileira");
+}else if(nacionalidade === "argentina"){
+	console.log("A pessoa é argentina ");
+}else if(nacionalidade === "uruguaia"){
+    console.log("A pessoa é uruguaia");
+}else if(nacionalidade === "chilena"){
+    console.log("A pessoa é chilena");
+}else if(nacionalidade === "colombiana"){
+	console.log("A pessoa é colombiana");
+}else{
+    console.log("Nacionalidade não encontrada");
+}
